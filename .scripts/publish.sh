318e289214e9a5ce09d48d981ad1d9a9e0e487e3
Sungyu Kang <gronxb@toss.im>
2025-05-28 19:34:22 +0900

    chore: changeset (#2)

--- .scripts/publish.sh
+++ .scripts/publish.sh
@@ -2,11 +2,4 @@
 
 set -e
 
-if [ -z "$NPM_TOKEN" ]; then
-  echo "Error: NPM_TOKEN environment variable is not set"
-  exit 1
-fi
-
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
-
 yarn workspaces foreach --all --no-private exec yarn npm publish --access public
