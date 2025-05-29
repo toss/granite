83bdff6c55fc3fc737e2ade7d41b85d42db79f21
HeeCheolKim <35059687+heecheolman@users.noreply.github.com>
2025-05-30 00:54:58 +0900

    fix: scaffold showcase (#19)
    * fix: scaffold showcase
    
    * chore: changeset
    
    * Update .changeset/brave-turtles-march.md
    
    ---------
    
    Co-authored-by: Sojin Park <raon0211@gmail.com>

--- packages/create-granite-app/templates/granite-app/pages/index.tsx
+++ packages/create-granite-app/templates/granite-app/pages/index.tsx
@@ -1,16 +1,1 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-import { createRoute } from '@granite-js/react-native';
-
-export const Route = createRoute('/', {
-  validateParams: (params) => params,
-  component: Index,
-});
-
-export function Index() {
-  return (
-    <View>
-      <Text>Hello Index</Text>
-    </View>
-  );
-}
+export { Route } from 'pages/index';
