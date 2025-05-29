8c7fda230d2bd9b0e15bdae508b4899a837d555c
Sojin Park <raon0211@toss.im>
2025-05-29 23:52:35 +0900

    useless file

--- services/bare/src/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { Bedrock, type InitialProps } from '@granite-js/react-native';
-import React from 'react';
-import { context } from '../require.context';
-
-function AppContainer({ children }: InitialProps) {
-  return <>{children}</>;
-}
-
-export default Bedrock.registerApp(AppContainer, { context, appName: 'bare' });
