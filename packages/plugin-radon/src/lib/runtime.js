7b6d72321293d3a9b8e235ae87e31295ce562369
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-07-24 12:26:56 +0900

    refact: delete 0.73+

--- packages/plugin-radon/src/lib/runtime.js
+++ packages/plugin-radon/src/lib/runtime.js
@@ -4,15 +4,6 @@ globalThis.__RADON_INSPECTOR_BRIDGE__ = require("./inspector_bridge")
 
 global.__RNIDE_enabled = true;
 
-// global.__RNIDE_register_navigation_plugin = function (name, plugin) {
-//   require("./wrapper.jsx").registerNavigationPlugin(name, plugin);
-// };
-
-// global.__RNIDE_register_dev_plugin = function (name) {
-//   require("./wrapper.jsx").registerDevtoolPlugin(name);
-// };
-
-
 const RNInternals = require("./vendor/rn-internals/rn-internals-0.72");
 const AppRegistry = RNInternals.AppRegistry;
 
