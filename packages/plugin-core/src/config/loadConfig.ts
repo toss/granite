adb9aaaed01765637e2c66b36ac5b10d9eecb5b0
leegeunhyeok <dev.ghlee@gmail.com>
2025-07-30 13:10:54 +0900

    fix

--- packages/plugin-core/src/config/loadConfig.ts
+++ packages/plugin-core/src/config/loadConfig.ts
@@ -1,9 +1,9 @@
-import type { CompleteGraniteConfig } from '@granite-js/plugin-core';
 import { getPackageRoot } from '@granite-js/utils';
 import { cosmiconfig } from 'cosmiconfig';
 import { TypeScriptLoader } from 'cosmiconfig-typescript-loader';
 import { assert } from 'es-toolkit';
 import type { defineConfig } from './defineConfig';
+import type { CompleteGraniteConfig } from '../schema/pluginConfig';
 
 const MODULE_NAME = 'granite';
 
