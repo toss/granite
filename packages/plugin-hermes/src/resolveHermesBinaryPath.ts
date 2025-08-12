6bb0216759b6070a18ea6e0865024b1528018e54
gronxb <gronxb@toss.im>
2025-08-12 19:23:55 +0900

    fix: hermes

--- packages/plugin-hermes/src/resolveHermesBinaryPath.ts
+++ packages/plugin-hermes/src/resolveHermesBinaryPath.ts
@@ -1,4 +1,5 @@
 import { type as osType } from 'os';
+import path from 'path';
 import { getPackageRoot } from '@granite-js/utils';
 
 const BINARY_PATH = {
@@ -6,6 +7,40 @@ const BINARY_PATH = {
   Linux: 'react-native/sdks/hermesc/linux64-bin/hermesc',
   Windows_NT: 'react-native/sdks/hermesc/win64-bin/hermesc.exe',
 } as const;
+
+/**
+ * Returns the Hermes OS binary folder name for the current platform.
+ */
+function getHermesOSBin(): string {
+  switch (process.platform) {
+    case 'win32':
+      return 'win64-bin';
+    case 'darwin':
+      return 'osx-bin';
+    default:
+      return 'linux64-bin';
+  }
+}
+
+/**
+ * Returns the Hermes executable name for the current platform.
+ */
+function getHermesOSExe(): string {
+  const hermesExecutableName = 'hermesc';
+  return process.platform === 'win32' ? `${hermesExecutableName}.exe` : hermesExecutableName;
+}
+
+function getReactNativePackagePath(root: string): string {
+  try {
+    return path.dirname(
+      require.resolve('react-native/package.json', {
+        paths: [root],
+      })
+    );
+  } catch {
+    return path.join('node_modules', 'react-native');
+  }
+}
 
 export function resolveHermesBinaryPath() {
   const root = getPackageRoot();
@@ -16,5 +51,5 @@ export function resolveHermesBinaryPath() {
     throw new Error(`Unsupported OS: ${os}`);
   }
 
-  return require.resolve(binaryPath, { paths: [root] });
+  return path.join(getReactNativePackagePath(root), 'sdks', 'hermesc', getHermesOSBin(), getHermesOSExe());
 }
