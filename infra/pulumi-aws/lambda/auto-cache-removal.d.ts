6effccba984160852de01513ae6285e5ff2f5ce0
Geunhyeok LEE <dev.ghlee@gmail.com>
2025-08-22 11:23:31 +0900

    fix: update package.json to supports any moduleResolutions (#106)
    * fix(plugin-micro-frontend): add d.ts
    
    * add changeset
    
    * update
    
    * fix packages meta

---
+++ infra/pulumi-aws/lambda/auto-cache-removal.d.ts
@@ -0,0 +1,1 @@
+export * from '../dist/lambda/auto-cache-removal';
--- packages/react-native/tsup.config.ts
+++ packages/react-native/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/config.ts', 'src/cli.ts', 'src/jest.ts'],
+  entry: ['src/config.ts', 'src/cli.ts', 'src/jest.ts', 'src/async-bridges.ts', 'src/constant-bridges.ts'],
   outDir: 'dist',
   format: ['esm', 'cjs'],
   dts: true,
