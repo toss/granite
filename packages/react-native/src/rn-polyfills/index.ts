41482779822d00ea1587fcfb3384fd8e01d5ac38
Geunhyeok LEE <dev.ghlee@gmail.com>
2025-10-13 21:23:34 +0900

    refactor: update default polyfills (#131)

--- packages/react-native/src/rn-polyfills/index.ts
+++
@@ -1,7 +0,0 @@
-import { setup as setupSymbolAsyncIterator } from './symbol-asynciterator';
-import { setup as setupURLPolyfill } from './url';
-
-export function setupPolyfills() {
-  setupSymbolAsyncIterator();
-  setupURLPolyfill();
-}
