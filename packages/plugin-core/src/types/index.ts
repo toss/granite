d23886a60234be4a27a3f4b95b3a18941a172898
Geunhyeok LEE <dev.ghlee@gmail.com>
2025-11-14 15:26:27 +0900

    feat: expose resolveRequest option (#154)
    * feat: expose resolveRequest option
    
    * add changeset

--- packages/plugin-core/src/types/index.ts
+++ packages/plugin-core/src/types/index.ts
@@ -3,3 +3,4 @@ export * from './BuildResult';
 export * from './DevServerConfig';
 export * from './GranitePlugin';
 export * from './MetroConfig';
+export type { ResolutionContext as MetroResolutionContext } from './vendors/metro';
