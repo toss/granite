449c84fb188f56475b451d671e5be5aec3d2605c
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2025-12-17 02:59:07 +0900

    chore: delete unnecessary reporter

--- packages/plugin-radon/src/index.ts
+++ packages/plugin-radon/src/index.ts
@@ -1,7 +1,5 @@
 export { radon } from './radonPlugin';
 
-export { radonCore } from './radonCorePlugin';
+export { radonCorePlugin } from './radonCorePlugin';
 
 export { radonGraniteRouter } from './radonGraniteRouterPlugin';
-
-export { radonCustomMetroPlugin } from './radonCustomMetroPlugin';
--- packages/plugin-radon/src/radonPlugin.ts
+++ packages/plugin-radon/src/radonPlugin.ts
@@ -1,14 +1,12 @@
 import type { GranitePlugin } from '@granite-js/plugin-core';
-import { radonCore } from './radonCorePlugin.js';
-import { radonCustomMetroPlugin } from './radonCustomMetroPlugin.js';
+import { radonCorePlugin } from './radonCorePlugin.js';
 import { radonGraniteRouter } from './radonGraniteRouterPlugin.js';
 
 export const radon = (): GranitePlugin[] => {
 
   const plugins: GranitePlugin[] = [];
 
-  plugins.push(radonCustomMetroPlugin());
-  plugins.push(radonCore());
+  plugins.push(radonCorePlugin());
   plugins.push(radonGraniteRouter());
 
   return plugins;
