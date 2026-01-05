cdcc859b5d936cc11d30204162ac50908d990f55
jingjing2222 <gudwjd1208@koreatech.ac.kr>
2026-01-05 15:34:59 +0900

    feat: seperate

--- packages/mpack/src/operations/serve.ts
+++ packages/mpack/src/operations/serve.ts
@@ -8,6 +8,7 @@ import { getMetroConfig } from '../metro/getMetroConfig';
 import { printLogo } from '../utils/printLogo';
 import { printServerUrl } from '../utils/printServerUrl';
 import { getModule } from '../vendors';
+import { keyReporter } from './keyReporter';
 
 const debug = Debug('cli:start');
 
@@ -53,38 +54,6 @@ export async function runServer({
   const driver = createPluginHooksDriver(config);
   const terminal = new Terminal(process.stdout);
   const terminalReporter = new TerminalReporter(terminal);
-  const keyReporter = {
-    update(event: { type: string; [key: string]: unknown }) {
-      switch (event.type) {
-        case 'unstable_server_log': {
-          const level = typeof event.level === 'string' ? event.level : 'info';
-          const data = event.data;
-          const output =
-            typeof data === 'string' ? data : Array.isArray(data) ? data.map(String).join(' ') : String(data);
-
-          if (level === 'error') {
-            console.error(output);
-          } else if (level === 'warn') {
-            console.warn(output);
-          } else {
-            console.log(output);
-          }
-          break;
-        }
-        case 'unstable_server_menu_updated': {
-          const message = event.message;
-          if (typeof message === 'string') {
-            console.log(message);
-          }
-          break;
-        }
-        case 'unstable_server_menu_cleared':
-          break;
-        default:
-          break;
-      }
-    },
-  };
 
   const resolvedConfig = await resolveConfig(config);
   const { middlewares = [], inspectorProxy, ...additionalMetroConfig } = resolvedConfig?.metro ?? {};
