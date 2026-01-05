b546c17cfe37a5c281c1cd102414e50101389eb2
Geunhyeok LEE <dev.ghlee@gmail.com>
2026-01-05 22:22:40 +0900

    refactor: move `asyncIterator` polyfill to runtime (#178)
    * refactor: move asyncIterator polyfill to runtime
    
    * add changeset

--- packages/mpack/src/bundler/internal/presets.ts
+++ packages/mpack/src/bundler/internal/presets.ts
@@ -44,18 +44,7 @@ export function combineWithBaseBuildConfig(
         define: defineGlobalVariables({ dev: context.dev }),
         prelude: getReactNativeSetupScripts({ rootDir: context.rootDir }),
         banner: {
-          js: [
-            globalVariables({ dev: context.dev }),
-            /**
-             * Polyfill for `@swc/helpers` build compatibility
-             *
-             * @see https://github.com/swc-project/swc/blob/v1.4.15/packages/helpers/esm/_async_iterator.js#L3
-             *
-             * - babel: No runtime issues after build as there is a fallback for `Symbol.asyncIterator`
-             * - swc: No fallback for `Symbol.asyncIterator`, so it needs to be defined in advance
-             */
-            `(function(){if(typeof Symbol!=="undefined"&&!Symbol.asyncIterator){Symbol.asyncIterator=Symbol.for("@@asyncIterator")}})();`,
-          ].join('\n'),
+          js: globalVariables({ dev: context.dev }),
         },
       },
       babel: {
