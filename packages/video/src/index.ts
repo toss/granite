e3356708d9a0e93ffd227c517adfab1befb085ed
gronxb <gronxb@toss.im>
2026-01-14 17:16:36 +0900

    feat: compat

--- packages/video/src/index.ts
+++ packages/video/src/index.ts
@@ -1,13 +1,9 @@
-export {
-  GraniteVideo,
-  GraniteVideo as Video,
-  GraniteVideo as default,
-  clearCache,
-  getWidevineLevel,
-  isCodecSupported,
-  isHEVCSupported,
-} from './GraniteVideo';
+import { Video } from './GraniteVideo';
+
+export { Video, clearCache, getWidevineLevel, isCodecSupported, isHEVCSupported } from './GraniteVideo';
 
 export * from './types';
 
 export type { NativeProps, NativeCommands } from './GraniteVideoNativeComponent';
+
+export default Video;
