f8201ead6db1f9d1f74a11c2f70d0567af9342ee
gronxb <gronxb@toss.im>
2026-01-19 15:07:50 +0900

    fix: types

--- packages/native/lottie-react-native.d.ts
+++ packages/native/lottie-react-native.d.ts
@@ -1,2 +1,5 @@
+import LottieView from '@granite-js/lottie';
+
 export * from '@granite-js/lottie';
-export { default } from '@granite-js/lottie';
+export { LottieView };
+export default LottieView;
