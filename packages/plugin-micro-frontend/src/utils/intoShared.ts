96f1b50dcbea996b6dafa86b1362511fbc3378aa
Sungyu Kang <gronxb@toss.im>
2026-01-26 19:01:59 +0900

    feat(mf): remote bundle not includes RN 81 and react core bundle (#190)
    * feat(mf): remote bundle not includes RN 81 and react core bundle
    
    * chore: bump 45
    
    * [autofix.ci] apply automated fixes
    
    * fix: core bundle
    
    * Revert "chore: bump 45"
    
    This reverts commit 7a2955a9e8a1b3673d38e1df434d9f208b492688.
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/plugin-micro-frontend/src/utils/intoShared.ts
+++ packages/plugin-micro-frontend/src/utils/intoShared.ts
@@ -2,12 +2,22 @@ import type { MicroFrontendPluginOptions, SharedConfig } from '../types';
 
 const SHARED_PRESETS: Record<string, string[]> = {
   'react-native': [
+    'react-native/Libraries/BatchedBridge/BatchedBridge',
+    'react-native/Libraries/BatchedBridge/NativeModules',
+    'react-native/Libraries/BatchedBridge/MessageQueue',
     'react-native/Libraries/NativeComponent/NativeComponentRegistry',
     'react-native/Libraries/NativeComponent/NativeComponentRegistryUnstable',
     'react-native/Libraries/NativeComponent/ViewConfigIgnore',
     'react-native/Libraries/ReactNative/RendererProxy',
+    'react-native/Libraries/StyleSheet/PlatformColorValueTypes',
+    'react-native/Libraries/StyleSheet/normalizeColor',
+    'react-native/Libraries/StyleSheet/processColor',
+    'react-native/Libraries/TurboModule/TurboModuleRegistry',
+    'react-native/Libraries/Utilities/NativePlatformConstantsIOS',
+    'react-native/Libraries/Utilities/Platform',
+    'react-native/Libraries/Utilities/defineLazyObjectProperty',
   ],
-  react: ['react/jsx-runtime'],
+  react: ['react/jsx-runtime', 'react/jsx-dev-runtime'],
 };
 
 export function intoShared(shared: MicroFrontendPluginOptions['shared']): SharedConfig | undefined {
