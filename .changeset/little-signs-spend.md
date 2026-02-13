cc75aa4678d72137af2ed99baf9046d8f5f0bcd8
Sungyu Kang <gronxb@toss.im>
2026-02-13 17:48:05 +0900

    fix(router): default header title should be empty string (#223)
    * fix: default header title should be empty string
    
    * chore: changeset

---
+++ .changeset/little-signs-spend.md
@@ -0,0 +1,6 @@
+---
+'@granite-js/react-native': patch
+'@granite-js/native': patch
+---
+
+fix: default header title should be empty string
--- packages/react-native/src/router/types/screen-option.ts
+++ packages/react-native/src/router/types/screen-option.ts
@@ -4,6 +4,7 @@ import { Platform } from 'react-native';
 export const DEFAULT_BACKGROUND_COLOR = '#ffffff';
 export const DEFAULT_HEADER_TINT_COLOR = '#191f28';
 export const BASE_STACK_NAVIGATOR_STYLE: NativeStackNavigationOptions = {
+  headerTitle: '',
   contentStyle: {
     backgroundColor: DEFAULT_BACKGROUND_COLOR,
   },
