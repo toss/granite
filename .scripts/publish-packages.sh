8a48b870a182df3d3609dfefa28c66636d48460c
gronxb <gronxb@toss.im>
2026-03-18 17:34:40 +0900

    Revert "chore: changeset release tag latest (#234)"
    This reverts commit a12915ce847d0c6ddb49b5eb7f4c45ea939bc5fd.

--- .scripts/publish-packages.sh
+++ .scripts/publish-packages.sh
@@ -7,7 +7,7 @@ while IFS= read -r line; do
   name=$(echo "$line" | jq -r '.name')
 
   echo "Publishing $name..."
-  if ! (cd "$location" && yarn npm publish --provenance --access public); then
+  if ! (cd "$location" && yarn npm publish --provenance --access public --tag next); then
     echo "Failed to publish $name, continuing..."
     failed=1
   fi
