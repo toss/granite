780e6ace5e428fa49b88bf0b7a7a1193372c3f1d
jingjing2222 <hj.jingjing2222@gmail.com>
2026-04-10 10:49:47 +0900

    fix: import

--- packages/react-native/src/keyboard/getInitialKeyboardHeight.ts
+++ packages/react-native/src/keyboard/getInitialKeyboardHeight.ts
@@ -1,4 +1,4 @@
-import { Platform } from "react-native";
+import { Platform, Keyboard } from "react-native";
 import { isNewArchEnabled } from "../utils/isNewArchEnabled";
 
 export function getInitialKeyboardHeight() {
