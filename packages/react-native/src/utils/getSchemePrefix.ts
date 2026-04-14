adfe616cb544d0a1dec2e109c3fa1339b81d3b7f
Sungyu Kang <gronxb@toss.im>
2026-04-14 14:11:39 +0900

    perf(react-native): without new URL optimize (#270)
    * perf(react-native): without new URL optimize
    
    * fix: changeset
    
    * without tag

--- packages/react-native/src/utils/getSchemePrefix.ts
+++ packages/react-native/src/utils/getSchemePrefix.ts
@@ -1,5 +1,3 @@
 export function getSchemePrefix({ scheme, appName, host }: { scheme: string; appName: string; host: string }) {
-  const base = host.length > 0 ? `${scheme}://${host}/` : `${scheme}://`;
-
-  return new URL(`${base}${appName}`).toString();
+  return host.length > 0 ? `${scheme}://${host}/${appName}` : `${scheme}://${appName}`;
 }
