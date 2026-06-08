91070c91430a5b1b77f7e715d693072949350210
heecheolman <msn9974@gmail.com>
2026-06-08 14:02:34 +0900

    fix: getSchemeUri

--- packages/react-native/src/native-modules/natives/getSchemeUri.ts
+++ packages/react-native/src/native-modules/natives/getSchemeUri.ts
@@ -1,18 +1,10 @@
-import { GraniteBrownfieldModule } from '@granite-js/brownfield-module';
+import { GraniteBrownfieldModule } from "@granite-js/brownfield-module";
 
 export function getSchemeUri() {
   try {
-    const graniteBrownfieldModule = GraniteBrownfieldModule as typeof GraniteBrownfieldModule & {
-      getSchemeUri?: () => string;
-    };
-
-    if (graniteBrownfieldModule.getSchemeUri != null) {
-      return graniteBrownfieldModule.getSchemeUri();
-    }
+    return GraniteBrownfieldModule.getSchemeUri();
   } catch {
     // Fallback to the deprecated `schemeUri` constant for older versions of the native module
+    return GraniteBrownfieldModule.getConstants().schemeUri;
   }
-
-  // Fallback to the deprecated `schemeUri` constant for older versions of the native module
-  return GraniteBrownfieldModule.getConstants().schemeUri;
 }
